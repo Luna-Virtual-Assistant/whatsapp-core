a9dc0fca28c233892ee7fbe7b8399f20ae12bc2a
todpig <luccasraffael6@gmail.com>
2024-04-07 20:10:05 -0300

    feat: solve conversation

--- routes.js
+++ routes.js
@@ -23,13 +23,16 @@ const listeners = {
 
 routes.get("/new-session", async (req, res) => {
   if (!req.query.sessionName) {
-    res.statusCode = 400;
-    return res.end(JSON.stringify({ res: "Missing sessionName in query" }));
+    return res
+      .status(400)
+      .end(JSON.stringify({ res: "Missing sessionName in query" }));
   }
   sessionName = req.query.sessionName;
   if (workers[sessionName]) {
     res.statusCode = 400;
-    return res.end(JSON.stringify({ res: "Session already exists" }));
+    return res
+      .status(400)
+      .end(JSON.stringify({ res: "Session already exists" }));
   }
   workers[sessionName] = new Worker("./whatsapp/bot.js", {
     argv: [sessionName],
@@ -38,9 +41,8 @@ routes.get("/new-session", async (req, res) => {
     const signal = message.signal;
     const value = message.value;
     if (signal == "qr-code") {
-      res.statusCode = 200;
       const { qr, code } = await listeners[signal](value);
-      return res.end(
+      return res.status(200).end(
         JSON.stringify({
           qr,
           code,
@@ -54,9 +56,11 @@ routes.get("/new-session", async (req, res) => {
 routes.post("/send-message", async (req, res) => {
   const { chatName, message } = req.body;
   if (!chatName || !message) {
-    return res.end(
-      JSON.stringify({ res: "Missing chatName or message in request body" })
-    );
+    return res
+      .status(400)
+      .end(
+        JSON.stringify({ res: "Missing chatName or message in request body" })
+      );
   }
   workers[sessionName].postMessage({
     signal: "send-message-by-chat-name",
@@ -65,8 +69,7 @@ routes.post("/send-message", async (req, res) => {
   workers[sessionName].on("message", (message) => {
     const { signal, value } = message;
     if (signal == "any") {
-      res.statusCode = value.status;
-      return res.end(JSON.stringify({ res: value.res }));
+      return res.status(value.status).end(JSON.stringify({ res: value.res }));
     }
   });
 });
@@ -76,12 +79,12 @@ routes.get("/get-all-chats", (req, res) => {
   workers[sessionName].on("message", (message) => {
     const { signal, value } = message;
     if (signal == "all-chats") {
-      res.statusCode = 200;
-      return res.end(JSON.stringify({ count: value.length, chats: value }));
+      return res
+        .status(200)
+        .end(JSON.stringify({ count: value.length, chats: value }));
     }
     if (signal == "any") {
-      res.statusCode = value.status;
-      return res.end(JSON.stringify({ res: value.res }));
+      return res.status(value.status).end(JSON.stringify({ res: value.res }));
     }
   });
 });
